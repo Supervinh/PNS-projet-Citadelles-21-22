47ce14e249449ba693b987a4d7800fd4a0985519
AureliaChabanier <aurelia.chabanier@etu.unice.fr>
2021-11-17 17:28:09 +0100

    Refactor de la classe MoteurDeJeu

--- src/main/java/fr/unice/polytech/MoteurDeJeu.java
+++ src/main/java/fr/unice/polytech/MoteurDeJeu.java
@@ -15,49 +15,64 @@ public class MoteurDeJeu {
     public static int carteAPiocher = 1;
     public static int nombre2QuartiersAConstruire = 8;
     public static Scanner sc = new Scanner(System.in);
+    public ArrayList<Joueur> joueurs = new ArrayList<>();
+
+    public MoteurDeJeu() {
+        System.out.println(this.hello());
+        System.out.println(deck);
+        // Implémentation des joueurs
+        this.initialiseJoueur();
+
+        int nb2Tours = 0;
+
+        while (this.pasFini()) { //Tant qu'un joueur n'a pas construit 8 quartiers, le jeu continue
+            System.out.println("\n##### Tour " + ++nb2Tours + " #####");
+            this.joueurs.forEach(Joueur::piocherPersonnage);
+            for (Joueur joueur : this.joueurs) { //Chaque joueurs jouent les uns après les autres
+                this.tour2Jeu(joueur);
+            }
+            this.joueurs.forEach(joueur -> deck.ajoutePersonnage(joueur.getPersonnage())); //Remet les cartes personnages dans le Deck
+            System.out.println("\n" + this.joueurs);
+        }
+        this.obtenirGagnant();
+
+    }
 
     public static void pause(int x) throws InterruptedException {
         TimeUnit.MILLISECONDS.sleep(x);
     }
 
-
-    public static String hello() {
+    public String hello() {
         return "Citadelle Grp.H - Jeux entre Bots";
     }
 
-    public MoteurDeJeu() {
-        System.out.println(MoteurDeJeu.hello());
-        System.out.println(deck);
-
-        // Creations des 'nombre2Joueur' Joueurs
-        ArrayList<Joueur> list2Joueurs = new ArrayList<>();
+    public void initialiseJoueur() {
         System.out.println("\n### Entrez Nom des Joueurs ###");
         for (int i = 1; i <= MoteurDeJeu.nombre2Joueur; i++) {
             System.out.println(" - Joueur " + i + ": CPU" + i);
-            list2Joueurs.add(new Joueur("CPU" + i));
+            this.joueurs.add(new Joueur("CPU" + i));
         }
+    }
 
-        int nb2Tours = 0;
-        while (list2Joueurs.stream().anyMatch(joueur -> joueur.getQuartiersConstruits().size() < MoteurDeJeu.nombre2QuartiersAConstruire)) {
-            System.out.println("\n##### Tour " + ++nb2Tours + " #####");
-            list2Joueurs.forEach(Joueur::piocherPersonnage);
-            for (Joueur joueur : list2Joueurs) {
-                System.out.println("\n### Tour de " + joueur.getNom() + " ###");
-                // Actions a faire
+    public boolean pasFini(){
+        return(this.joueurs.stream().anyMatch(joueur -> joueur.getQuartiersConstruits().size() < MoteurDeJeu.nombre2QuartiersAConstruire));
+    }
 
-                if (joueur.getQuartiers().size() == 0) {
-                    joueur.piocherQuartier();
-                } else {
-                    joueur.piocherOr();
-                }
-                joueur.construireQuartier();
-            }
-            list2Joueurs.forEach(joueur -> deck.ajoutePersonnage(joueur.getPersonnage()));
-            System.out.println("\n" + list2Joueurs);
+    public void tour2Jeu(Joueur joueur){
+        System.out.println("\n### Tour de " + joueur.getNom() + " ###");
+        // Actions a faire
+
+        if (joueur.getQuartiers().size() == 0) {
+            joueur.piocherQuartier();
+        } else {
+            joueur.piocherOr();
         }
-        list2Joueurs.forEach(Joueur::calculePoints);
-        int maxScore = list2Joueurs.stream().mapToInt(Joueur::getPoints).max().orElse(0);
-        Joueur winner = list2Joueurs.stream().filter(joueur -> joueur.getPoints() == maxScore).findFirst().orElse(null);
+        joueur.construireQuartier();
+    }
+    public void obtenirGagnant(){
+        this.joueurs.forEach(Joueur::calculePoints);
+        int maxScore = this.joueurs.stream().mapToInt(Joueur::getPoints).max().orElse(0);
+        Joueur winner = this.joueurs.stream().filter(joueur -> joueur.getPoints() == maxScore).findFirst().orElse(null);
         if (winner != null) {
             System.out.println("\nLe Gagnant est: " + winner.getNom() + " avec " + winner.getPoints() + " points");
         } else {
