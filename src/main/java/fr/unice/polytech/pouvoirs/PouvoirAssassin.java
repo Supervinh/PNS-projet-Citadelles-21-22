5d8ef455665babb932185428af0ecaaf3f1c4af5
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-18 22:59:26 +0100

    Added the ability to kill someone for 1 turn

--- src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
+++ src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
@@ -1,9 +1,11 @@
 package fr.unice.polytech.pouvoirs;
 
+import fr.unice.polytech.Joueur;
+
 public class PouvoirAssassin implements IPouvoir {
 
     @Override
-    public void utiliserPouvoir() {
-        //assasin
+    public void utiliserPouvoir(Joueur joueur, Joueur cible) {
+        joueur.tue(cible);
     }
 }
