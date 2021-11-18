686728ad3e456469552360e44d4b0634b20d6517
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-18 23:45:58 +0100

    Made it Possible to Run the Game as many times as you want (by fixed deck attribut) Added the functionality of showing multiple winners if there is a draw.

--- src/main/java/fr/unice/polytech/MoteurDeJeu.java
+++ src/main/java/fr/unice/polytech/MoteurDeJeu.java
@@ -8,8 +8,8 @@ import java.util.concurrent.TimeUnit;
 
 public class MoteurDeJeu {
 
-    public static Deck deck = new Deck();
-    public static int nombre2Joueur = 2;
+    public static Deck deck;
+    public static int nombre2Joueur = 5;
     public static int or2Depart = 2;
     public static int orAPiocher = 2;
     public static int carte2Depart = 4;
@@ -20,8 +20,9 @@ public class MoteurDeJeu {
     private int nb2Tours = 0;
 
     public MoteurDeJeu() {
+        deck = new Deck();
         System.out.println(this.hello());
-        System.out.println(deck);
+        System.out.println("\n" + deck);
 
         this.initialiseJoueur();
         while (this.pasFini()) {
@@ -34,7 +35,6 @@ public class MoteurDeJeu {
             System.out.println("\n" + this.joueurs);
         }
         this.obtenirGagnant();
-
     }
 
     public static void pause(int x) throws InterruptedException {
@@ -59,26 +59,19 @@ public class MoteurDeJeu {
 
     public void tour2Jeu(Joueur joueur) {
         System.out.println("\n" + cc.seperateur1() + CouleurConsole.RESET + "Tour de " + joueur.getNom() + cc.seperateur1());
-        System.out.println(joueur.getNom2Strategie());
         joueur.jouer();
-
-       /*if (joueur.getQuartiers().size() == 0) {
-            joueur.piocherQuartier();
-        } else {
-            joueur.piocherOr();
-        }
-        joueur.construireQuartier();*/
     }
 
     public void obtenirGagnant() {
         this.joueurs.forEach(Joueur::calculePoints);
         int maxScore = this.joueurs.stream().mapToInt(Joueur::getPoints).max().orElse(0);
-        Joueur winner = this.joueurs.stream().filter(joueur -> joueur.getPoints() == maxScore).findFirst().orElse(null);
-        if (winner != null) {
-            System.out.println("\nLe " + CouleurConsole.RED + "Gagnant" + CouleurConsole.RESET + " est: " + winner.getNom() + " avec " + CouleurConsole.YELLOW_BRIGHT + winner.getPoints() + CouleurConsole.RESET + " points");
-        } else {
-            System.out.println("\nPas de " + CouleurConsole.RED + "Gagnant" + CouleurConsole.RESET);
+        ArrayList<Joueur> winners = new ArrayList<>(this.joueurs.stream().filter(joueur -> joueur.getPoints() == maxScore).toList());
+        switch (winners.size()) {
+            case 1 -> System.out.print("\nLe " + CouleurConsole.RED + "Gagnant" + CouleurConsole.RESET + " est ");
+            case 0 -> System.out.println("\nPas de " + CouleurConsole.RED + "Gagnant" + CouleurConsole.RESET);
+            default -> System.out.println("\nLes " + CouleurConsole.RED + "Gagnants" + CouleurConsole.RESET + " sont: ");
         }
+        winners.forEach(winner -> System.out.println(winner.getNom() + " avec " + CouleurConsole.YELLOW_BRIGHT + winner.getPoints() + CouleurConsole.RESET + " points"));
         this.montrerClassement();
     }
 
