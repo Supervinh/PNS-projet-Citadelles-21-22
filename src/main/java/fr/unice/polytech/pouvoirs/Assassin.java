7d241f720af6bbde7db108fbd8a4df7707d993b4
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-18 22:32:22 +0100

    Renamed

--- src/main/java/fr/unice/polytech/pouvoirs/Assassin.java
+++
@@ -1,5 +0,0 @@
-package fr.unice.polytech.pouvoirs;
-
-public class Assassin {
-
-}
---
+++ src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.pouvoirs;
+
+public class PouvoirAssassin implements IPouvoir {
+
+    @Override
+    public void utiliserPouvoir() {
+        //assasin
+    }
+}
