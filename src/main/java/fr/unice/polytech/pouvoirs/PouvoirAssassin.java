9514aa6affc9b7593ac1cd99e3d8bb654d4b361c
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-19 03:23:02 +0100

    Added the Assassin's effect #11

--- src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
+++ src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
@@ -1,11 +1,13 @@
 package fr.unice.polytech.pouvoirs;
 
 import fr.unice.polytech.Joueur;
+import fr.unice.polytech.couleur.CouleurConsole;
 
 public class PouvoirAssassin implements IPouvoir {
 
     @Override
     public void utiliserPouvoir(Joueur joueur, Joueur cible) {
+        System.out.println(joueur.getNom() + " a " + CouleurConsole.RED + "tué " + CouleurConsole.RESET + cible.getNom());
         joueur.tue(cible);
     }
 }
