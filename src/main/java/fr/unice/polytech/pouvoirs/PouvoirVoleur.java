8de5f6fac2088e9b098c3fb2b06f65bf7655f9be
ClervieCauser <clervie.causer@etu.unice.fr>
2021-11-23 19:19:36 +0100

    Update pouvoir voleur #12

--- src/main/java/fr/unice/polytech/pouvoirs/PouvoirVoleur.java
+++ src/main/java/fr/unice/polytech/pouvoirs/PouvoirVoleur.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.pouvoirs;
 
+import fr.unice.polytech.CartePersonnage;
 import fr.unice.polytech.Joueur;
 import fr.unice.polytech.MoteurDeJeu;
 import fr.unice.polytech.couleur.CouleurConsole;
@@ -13,18 +14,29 @@ public class PouvoirVoleur implements IPouvoir {
     @Override
     public void utiliserPouvoir(Joueur joueur) {
 
-        ArrayList<Joueur> cibles = new ArrayList<>(List.copyOf(MoteurDeJeu.joueurs));
-        cibles.remove(joueur);
-        Joueur cible = cibles.get(new Random().nextInt(cibles.size()));
+        // Choix de Cible utilisant un nom de Personnage
+        ArrayList<CartePersonnage> cibles = new ArrayList<>(List.copyOf(MoteurDeJeu.deck.getPersonnagesPossibles()));
+        cibles.remove(joueur.getPersonnage());
+        cibles.removeIf(p-> p.getNom().equals("Assassin"));
+        CartePersonnage cibleNomPersonnage = cibles.get(new Random().nextInt(cibles.size()));
 
-        while (cible.getPersonnage().getNom().equals("Assassin") || cible.isEstTue()) {
-            cible = cibles.get(new Random().nextInt(cibles.size()));
+        // Si Cible est attribuée a un Joueur ou pas
+        Joueur cible = MoteurDeJeu.joueurs.stream()
+                .filter(j -> j.getPersonnage().equals(cibleNomPersonnage))
+                .findFirst()
+                .orElse(null);
+
+
+
+        if (cible != null && !cible.isEstTue()) {
+            joueur.ajouteOr(cible.getOr());
+
+            System.out.println(joueur.getNom() + " a volé " + CouleurConsole.printGold(String.valueOf(cible.getOr())) + " pièces d'or à " + cible.getNom());
+
+            cible.setOr(0);
+        } else {
+            System.out.println(joueur.getNom() + " a essayé de voler des pièces d'or à " + cibleNomPersonnage.getNom());
         }
 
-        joueur.ajouteOr(cible.getOr());
-
-        System.out.println(joueur.getNom() + " a voler " + CouleurConsole.printGold(String.valueOf(cible.getOr())) + " à " + cible.getNom());
-
-        cible.setOr(0);
     }
 }
