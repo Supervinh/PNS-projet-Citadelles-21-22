01be89fadc68715c6a49eb944a1553816cf33e77
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-23 01:16:24 +0100

    Fixed tests

--- src/test/java/fr/unice/polytech/JoueurTest.java
+++ src/test/java/fr/unice/polytech/JoueurTest.java
@@ -1,60 +1,67 @@
 package fr.unice.polytech;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JoueurTest {
-    @Test
-    void notEmpty(){
-        Joueur j1= new Joueur("1");
-        assertEquals(MoteurDeJeu.or2Depart,j1.getOr());
-        assertNotEquals(null,j1.getQuartiers());
+
+    @BeforeEach
+    void setup() {
+        new MoteurDeJeu();
     }
 
     @Test
-    void aPiocheOr(){
-        Joueur j2=new Joueur("2");
+    void notEmpty() {
+        Joueur j1 = new Joueur("1");
+        assertEquals(MoteurDeJeu.or2Depart, j1.getOr());
+        assertNotEquals(null, j1.getQuartiers());
+    }
+
+    @Test
+    void aPiocheOr() {
+        Joueur j2 = new Joueur("2");
         j2.piocherOr();
-        assertNotEquals(MoteurDeJeu.or2Depart,j2.getOr());
-        assertEquals(MoteurDeJeu.orAPiocher+MoteurDeJeu.or2Depart, j2.getOr());
+        assertNotEquals(MoteurDeJeu.or2Depart, j2.getOr());
+        assertEquals(MoteurDeJeu.orAPiocher + MoteurDeJeu.or2Depart, j2.getOr());
     }
 
     @Test
-    void nomCorrect(){
-        Joueur j3=new Joueur("3");
+    void nomCorrect() {
+        Joueur j3 = new Joueur("3");
         assertEquals("3", j3.getNom());
-        Joueur j4=new Joueur(null);
-        assertEquals(null,j4.getNom());
+        Joueur j4 = new Joueur(null);
+        assertNull(j4.getNom());
     }
 
     @Test
-    void piocherPersoDifferents(){
-        Joueur j1=new Joueur("1");
-        Joueur j2=new Joueur("2");
+    void piocherPersoDifferents() {
+        Joueur j1 = new Joueur("1");
+        Joueur j2 = new Joueur("2");
         j1.piocherPersonnage();
         j2.piocherPersonnage();
-        assertNotEquals(j1.getPersonnage(),j2.getPersonnage());
+        assertNotEquals(j1.getPersonnage(), j2.getPersonnage());
     }
 
     @Test
-    void piocherCartesDifferentes(){
-        Joueur j1=new Joueur("1");
-        Joueur j2=new Joueur("2");
+    void piocherCartesDifferentes() {
+        Joueur j1 = new Joueur("1");
+        Joueur j2 = new Joueur("2");
         j1.piocherQuartier();
         j2.piocherQuartier();
-        assertNotEquals(j1.getQuartiers(),j2.getQuartiers());
+        assertNotEquals(j1.getQuartiers(), j2.getQuartiers());
     }
 
     @Test
-    void pointsCorrect(){
-        Joueur j1=new Joueur("1");
-        assertEquals(0,j1.getPoints());
+    void pointsCorrect() {
+        Joueur j1 = new Joueur("1");
+        assertEquals(0, j1.getPoints());
     }
 
     @Test
-    void estRoi(){
-        Joueur j1=new Joueur("1");
+    void estRoi() {
+        Joueur j1 = new Joueur("1");
         assertFalse(j1.isEstRoi());
     }
 }
