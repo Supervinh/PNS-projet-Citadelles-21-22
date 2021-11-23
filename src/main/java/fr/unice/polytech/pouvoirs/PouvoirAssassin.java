ea33eb598fe1ad1a1ae3ce70c04a0fa4741f94d2
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-23 18:44:45 +0100

    Fixed target (can now be a non-picked character) #11

--- src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
+++ src/main/java/fr/unice/polytech/pouvoirs/PouvoirAssassin.java
@@ -1,23 +1,30 @@
 package fr.unice.polytech.pouvoirs;
 
+import fr.unice.polytech.CartePersonnage;
+import fr.unice.polytech.CarteQuartier;
 import fr.unice.polytech.Joueur;
 import fr.unice.polytech.MoteurDeJeu;
 import fr.unice.polytech.couleur.CouleurConsole;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class PouvoirAssassin implements IPouvoir {
 
     @Override
     public void utiliserPouvoir(Joueur joueur) {
 
-        ArrayList<Joueur> cibles = new ArrayList<>(List.copyOf(MoteurDeJeu.joueurs));
-        cibles.remove(joueur);
-        Joueur cible = cibles.get(new Random().nextInt(cibles.size()));
+        // Choix de Cible utilisant un nom de Personnage
+        ArrayList<CartePersonnage> cibles = new ArrayList<>(List.copyOf(MoteurDeJeu.deck.getPersonnagesPossibles()));
+        cibles.remove(joueur.getPersonnage());
+        CartePersonnage cibleNomPersonnage = cibles.get(new Random().nextInt(cibles.size()));
 
-        System.out.println(joueur.getNom() + " a " + CouleurConsole.printRed("tué ") + cible.getNom());
-        joueur.tue(cible);
+        // Si Cible est attribuée a un Joueur ou pas
+        Joueur cible = MoteurDeJeu.joueurs.stream().filter(j -> j.getPersonnage().equals(cibleNomPersonnage)).findFirst().orElse(null);
+        if (cible != null) {
+            System.out.println(joueur.getNom() + " a " + CouleurConsole.printRed("tué " + cible.getNom()));
+            joueur.tue(cible);
+        } else {
+            System.out.println(joueur.getNom() + " a essayé de " + CouleurConsole.printRed("tuer ") + cibleNomPersonnage.getArticle() + cibleNomPersonnage.getNom());
+        }
     }
 }
