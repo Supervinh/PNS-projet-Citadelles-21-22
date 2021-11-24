72f285cc8fdb73bb90cf1669ba6656a12b9640ea
unknown <narutovinh@hotmail.fr>
2021-11-24 16:02:15 +0100

    Ajout classe PouvoirArchitecteTest #17

---
+++ src/test/java/fr/unice/polytech/PouvoirArchitecteTest.java
@@ -0,0 +1,44 @@
+package fr.unice.polytech;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PouvoirArchitecteTest {
+    Joueur joueur;
+    MoteurDeJeu moteurDeJeu;
+    Strategie strategie;
+    ArrayList<Joueur> joueurs = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        moteurDeJeu = new MoteurDeJeu();
+        moteurDeJeu.initialiseJoueurs(joueurs);
+        joueur = joueurs.get(1);
+        joueur.setPersonnage(new CartePersonnage(7,"Architecte","Aucun","L'Architecte pioche deux cartes quartier en plus. il peut bâtir jusqu'à trois quartiers."));
+        strategie = new Strategie(joueur);
+        strategie.actionPersonnage();
+    }
+
+    @Test
+    void piocheQuartierTest(){
+        System.out.println("Pioche \n");
+        joueur.setQuartiers(new ArrayList<CarteQuartier>());
+        joueur.setOr(0);
+        strategie.prochainTour();
+        assertEquals(joueur.getQuartiers().size(),2);
+    }
+
+    @Test
+    void construire3Quartiers(){
+        System.out.println("Construction \n");
+        joueur.setOr(100);
+        strategie.prochainTour();
+        assertEquals(joueur.getQuartiersConstruits().size(),3);
+    }
+
+}
