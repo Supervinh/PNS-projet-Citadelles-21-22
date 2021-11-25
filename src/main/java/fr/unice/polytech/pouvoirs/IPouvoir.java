e3f68b8dc0b60875e4595c6809d33e19c90266cd
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-11-25 12:14:50 +0100

    Moved Pay Taxes to Interface default method

--- src/main/java/fr/unice/polytech/pouvoirs/IPouvoir.java
+++ src/main/java/fr/unice/polytech/pouvoirs/IPouvoir.java
@@ -1,7 +1,16 @@
 package fr.unice.polytech.pouvoirs;
 
 import fr.unice.polytech.Joueur;
+import fr.unice.polytech.couleur.CouleurConsole;
 
 public interface IPouvoir {
     void utiliserPouvoir(Joueur joueur);
+
+    default void recupererTaxes(Joueur joueur) {
+        System.out.println(CouleurConsole.printRed("| ") + "\n" + CouleurConsole.printRed("| ") + "Recuperation des " + CouleurConsole.printGold("Taxes") + " des Gemmes " + CouleurConsole.printPurple(joueur.getPersonnage().getGemme()));
+        int count = (int) joueur.getQuartiersConstruits().stream().filter(quartier -> quartier.getGemme().equals(joueur.getPersonnage().getGemme())).count();
+        joueur.ajouteOr(count);
+        boolean plurielle = count > 1;
+        System.out.println(CouleurConsole.printRed("| ") + joueur.getNom() + " a pioché " + CouleurConsole.printGold("" + count) + " pièce" + (plurielle ? "s" : "") + " d'" + CouleurConsole.printGold("or") + " supplémentaire" + (plurielle ? "s" : "") + ".");
+    }
 }
