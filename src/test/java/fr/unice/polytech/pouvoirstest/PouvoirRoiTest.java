3c478a58447285839577a9bd26eb121f12d9d648
unknown <narutovinh@hotmail.fr>
2021-11-29 14:13:16 +0100

    Création de PouvoirRoiTest #28

---
+++ src/test/java/fr/unice/polytech/pouvoirstest/PouvoirRoiTest.java
@@ -0,0 +1,44 @@
+package fr.unice.polytech.pouvoirstest;
+
+import fr.unice.polytech.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class PouvoirRoiTest {
+    Joueur joueur;
+    MoteurDeJeu moteurDeJeu;
+    Strategie strategie;
+    ArrayList<Joueur> joueurs = new ArrayList<>();
+    ArrayList<CarteQuartier> quartiers = new ArrayList<>();
+    CarteQuartier quartier;
+
+    @BeforeEach
+    void setUp() {
+        moteurDeJeu = new MoteurDeJeu();
+        moteurDeJeu.initialiseJoueurs(joueurs);
+        joueur = joueurs.get(1);
+        joueur.setPersonnage(new CartePersonnage(4, "Roi", "Noblesse", "Le roi prend la carte Couronne et choisira en premier son personnage au prochain tour. Chaque quartier noble qu'il possède lui rapporte une pièce d'or."));
+        quartier = new CarteQuartier(5, "Manoir", "Noblesse", 3);
+        quartiers.add(quartier);
+        strategie = new Strategie(joueur);
+        strategie.actionPersonnage();
+    }
+
+    @Test
+    void taxesAjoutTest() {
+        joueur.setQuartiers(quartiers);
+        joueur.ajouteOr(1);
+        joueur.construireQuartier();
+        strategie.prochainTour();
+        assertTrue(joueur.isEstRoi());
+        assertEquals(3, joueur.getOr());
+
+    }
+
+}
