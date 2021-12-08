b372845c5efe5fcbcee2b12b7efc39bf5efd130f
unknown <narutovinh@hotmail.fr>
2021-12-08 17:08:51 +0100

    Modification de PouvoirMarchandTest #28

--- src/test/java/fr/unice/polytech/pouvoirstest/PouvoirMarchandTest.java
+++ src/test/java/fr/unice/polytech/pouvoirstest/PouvoirMarchandTest.java
@@ -1,8 +1,13 @@
 package fr.unice.polytech.pouvoirstest;
 
-import fr.unice.polytech.*;
+import fr.unice.polytech.CartePersonnage;
+import fr.unice.polytech.CarteQuartier;
+import fr.unice.polytech.Joueur;
+import fr.unice.polytech.MoteurDeJeu;
+import fr.unice.polytech.pouvoirs.PouvoirMarchand;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
@@ -10,31 +15,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class PouvoirMarchandTest {
-    Joueur joueur;
-    MoteurDeJeu moteurDeJeu;
-    Strategie strategie;
-    ArrayList<Joueur> joueurs = new ArrayList<>();
+    Joueur marchand;
     ArrayList<CarteQuartier> quartiers = new ArrayList<>();
     CarteQuartier quartier;
+    MoteurDeJeu moteurDeJeu;
+    ArrayList<Joueur> joueurs = new ArrayList<>();
+
 
     @BeforeEach
     void setUp() {
         moteurDeJeu = new MoteurDeJeu();
         moteurDeJeu.initialiseJoueurs(joueurs, true);
-        joueur = joueurs.get(1);
-        joueur.setPersonnage(new CartePersonnage(6, "Marchand", "Commerce et Artisanat", "Le Marchand reçoit une pièce d'or en plus au début de son tour. Chaque quartier marchand qu'il possède lui rapporte une pièce d'or."));
+        marchand = joueurs.get(0);
+        marchand.setPersonnage(new CartePersonnage(6, "Marchand", "Commerce et Artisanat", "Le Marchand reçoit une pièce d'or en plus au début de son tour. Chaque quartier marchand qu'il possède lui rapporte une pièce d'or."));
         quartier = new CarteQuartier(11.2, "Comptoir", "Commerce et Artisanat", 3);
         quartiers.add(quartier);
-        strategie = new Strategie(joueur);
     }
 
     @Test
     void taxesAjoutTest() {
-        joueur.setQuartiers(quartiers);
-        joueur.ajouteOr(1);
-        joueur.construireQuartier();
-        strategie.prochainTour();
-        assertEquals(2, joueur.getOr());
+        PouvoirMarchand taxe = Mockito.mock(PouvoirMarchand.class);
+        Mockito.doCallRealMethod().when(taxe).utiliserPouvoir(marchand);
+        Mockito.doCallRealMethod().when(taxe).recupererTaxes(marchand);
+        marchand.setQuartiers(quartiers);
+        marchand.ajouteOr(1);
+        marchand.construireQuartier();
+        taxe.utiliserPouvoir(marchand);
+        assertEquals(2, marchand.getOr());
+    }
 
+    @Test
+    void pasDeTaxe() {
+        PouvoirMarchand pasTaxe = Mockito.mock(PouvoirMarchand.class);
+        Mockito.doCallRealMethod().when(pasTaxe).utiliserPouvoir(marchand);
+        Mockito.doCallRealMethod().when(pasTaxe).recupererTaxes(marchand);
+        marchand.setOr(0);
+        pasTaxe.utiliserPouvoir(marchand);
+        assertEquals(1, marchand.getOr());
     }
 }
