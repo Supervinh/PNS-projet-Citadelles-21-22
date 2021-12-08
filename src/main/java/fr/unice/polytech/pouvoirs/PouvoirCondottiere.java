33e12a19d3017354ee519b6734dc7833362ebae4
Matis Herrmann <matis_herrmann@hotmail.fr>
2021-12-08 13:45:00 +0100

    Fix PouvoirCondottiere.java #36

--- src/main/java/fr/unice/polytech/pouvoirs/PouvoirCondottiere.java
+++ src/main/java/fr/unice/polytech/pouvoirs/PouvoirCondottiere.java
@@ -15,13 +15,13 @@ public class PouvoirCondottiere implements IPouvoir {
     public void utiliserPouvoir(Joueur joueur) {
 
         // Choix de Cible utilisant un Joueur
-        Joueur cible = cibleAleatoire(joueur);
+        Joueur cible = cibleAleatoire();
 
-        int numQuartier = choixQuartierAleatoire(joueur, cible);
-        CarteQuartier quartierDetruit = cible.getQuartiersConstruits().get(numQuartier);
+        CarteQuartier quartierDetruit = choixQuartierAleatoire(joueur, cible);
         System.out.println(CouleurConsole.printRed("| Pouvoir " + joueur.getPersonnage().getNom()));
-        if (hasEnoughMoney(joueur, quartierDetruit)) {
-            cible.getQuartiersConstruits().remove(numQuartier);
+        if (quartierDetruit != null) {
+            cible.getQuartiersConstruits().remove(quartierDetruit);
+            MoteurDeJeu.deck.ajouterQuartierDeck(quartierDetruit);
             joueur.setOr(-quartierDetruit.getPrix() + 1);
             System.out.println(CouleurConsole.printRed("| ") + joueur.getNom() + " a détruit le quartier " + CouleurConsole.printGreen(quartierDetruit.getNom()) + " de " + cible.getNom());
         } else {
@@ -30,22 +30,19 @@ public class PouvoirCondottiere implements IPouvoir {
         this.recupererTaxes(joueur);
     }
 
-    public Joueur cibleAleatoire(Joueur joueur) {
+    public Joueur cibleAleatoire() {
         ArrayList<Joueur> cibles = new ArrayList<>(List.copyOf(MoteurDeJeu.personnagesConnus));
-        cibles.removeIf(j -> (j.getPersonnage().getNom().equals("Évêque") &&  !j.isMort()) || j.getQuartiersConstruits().size() <= 0 || j.getQuartiersConstruits().size() >= MoteurDeJeu.nombre2QuartiersAConstruire);
+        cibles.removeIf(j -> (j.getPersonnage().getNom().equals("Évêque") && !j.isMort()) || j.getQuartiersConstruits().size() <= 0 || j.getQuartiersConstruits().size() >= MoteurDeJeu.nombre2QuartiersAConstruire);
         return cibles.get(new Random().nextInt(cibles.size()));
     }
 
-    public int choixQuartierAleatoire(Joueur joueur, Joueur cible) {
-        Random r = new Random();
-        int i = r.nextInt(cible.getQuartiersConstruits().size());
-        while(cible.getQuartiersConstruits().get(i).getNom().equals("Donjon")){
-            if(cible.getQuartiersConstruits().size()==1){
-                Joueur c2=cibleAleatoire(joueur);
-                while(cible==c2){
-                    cible=cibleAleatoire(joueur);}}
-            i = r.nextInt(cible.getQuartiersConstruits().size());}
-        return i;
+    public CarteQuartier choixQuartierAleatoire(Joueur joueur, Joueur cible) {
+        ArrayList<CarteQuartier> cibesPossible = new ArrayList<>(cible.getQuartiersConstruits().stream().filter(cq -> !cq.getNom().equals("Donjon") && hasEnoughMoney(joueur, cq)).toList());
+        if (cibesPossible.size() > 0) {
+            return cibesPossible.get(new Random().nextInt(cibesPossible.size()));
+        } else {
+            return null;
+        }
     }
 
     public boolean hasEnoughMoney(Joueur joueur, CarteQuartier quartier) {
