971f747b0e5d4535f2ae0d179c36ac38317bb0ab
unknown <narutovinh@hotmail.fr>
2022-01-03 11:37:28 +0100

    Ajout de la stratégie 'Agressif' #59

--- src/main/java/fr/unice/polytech/strategie/Agressif.java
+++ src/main/java/fr/unice/polytech/strategie/Agressif.java
@@ -1,39 +1,38 @@
 package fr.unice.polytech.strategie;
 
-import fr.unice.polytech.CartePersonnage;
-import fr.unice.polytech.CarteQuartier;
-import fr.unice.polytech.Joueur;
-import fr.unice.polytech.MoteurDeJeu;
+import fr.unice.polytech.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class RusherQuartiers implements IStrategie {
+public class Agressif implements IStrategie {
 
     @Override
     public CartePersonnage choixDePersonnage(Joueur joueur, ArrayList<CartePersonnage> personnages) {
-        CartePersonnage choix = personnages.stream().filter(cp -> cp.getNom().equals("Architecte")).findAny().orElseGet(
+        CartePersonnage choix = personnages.stream().filter(cp -> cp.getNom().equals("Assassin") || cp.getNom().equals("Voleur")).findAny().orElseGet(
                 () -> IStrategie.super.choixDePersonnage(joueur, personnages));
-        if (joueur.getOr() < 3) {
-            choix = personnages.stream().filter(cp -> cp.getNom().equals("Voleur")).findAny().orElse(choix);
-        }
         return choix;
     }
 
     @Override
     public CartePersonnage choixDeCibleCartePersonnage(Joueur joueur, ArrayList<CartePersonnage> ciblesTemp) {
-        MoteurDeJeu.joueurs.forEach(Joueur::calculePoints);
-        int scoreMax = MoteurDeJeu.joueurs.stream().filter(j -> j != joueur).mapToInt(Joueur::getPoints).max().orElse(0);
-        Joueur cibleJoueur = MoteurDeJeu.joueurs.stream().filter(j -> j.getPoints() == scoreMax).findFirst().orElse(null);
-        if (cibleJoueur != null) {
-            return cibleJoueur.getStrategie().getIStrategie().choixDePersonnage(joueur, MoteurDeJeu.deck.getPersonnagesPossibles());
-        } else {
+        CartePersonnage personnageCible = null;
+        if (joueur.getPersonnage().getNom().equals("Assassin")) {
+            personnageCible = MoteurDeJeu.deck.getPersonnages().stream().filter(p -> p.getNom().equals("Architecte") || p.getNom().equals("Magicien")).findFirst().orElse(null);
+        }
+        else if (joueur.getPersonnage().getNom().equals("Voleur")) {
+            personnageCible = MoteurDeJeu.deck.getPersonnages().stream().filter(p -> p.getNom().equals("Architecte") || p.getNom().equals("Marchand")).findFirst().orElse(null);
+        }
+        if (personnageCible == null) {
             return IStrategie.super.choixDeCibleCartePersonnage(joueur, ciblesTemp);
         }
+        return personnageCible;
     }
 
     @Override
     public Joueur choixDeCibleJoueur(Joueur joueur, ArrayList<Joueur> cibles) {
+        int nbreQuartierMain = MoteurDeJeu.joueurs.stream().filter(j -> j != joueur).map(Joueur::getQuartiers).mapToInt(ArrayList::size).max().orElse(0);
+        if (nbreQuartierMain>4) return MoteurDeJeu.joueurs.stream().filter(j -> j.getPoints() == nbreQuartierMain).findFirst().orElseGet(() -> IStrategie.super.choixDeCibleJoueur(joueur, cibles));
         MoteurDeJeu.joueurs.forEach(Joueur::calculePoints);
         int scoreMax = MoteurDeJeu.joueurs.stream().filter(j -> j != joueur).mapToInt(Joueur::getPoints).max().orElse(0);
         return MoteurDeJeu.joueurs.stream().filter(j -> j.getPoints() == scoreMax).findFirst().orElseGet(() -> IStrategie.super.choixDeCibleJoueur(joueur, cibles));
@@ -48,6 +47,6 @@ public class RusherQuartiers implements IStrategie {
 
     @Override
     public String nomStrategie() {
-        return "Quartiers les moins chers";
+        return "Stratégie aggressive afin de gêner au plus les adversaires";
     }
 }
