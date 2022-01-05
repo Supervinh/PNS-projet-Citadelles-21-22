ce7aef1e73bf325a96fb301a8a257f1e4bd80884
Matis Herrmann <matis_herrmann@hotmail.fr>
2022-01-05 16:10:32 +0100

    Rookie mistake #65

--- src/main/java/fr/unice/polytech/Main.java
+++ src/main/java/fr/unice/polytech/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         MoteurDeJeu mj;
-        MoteurDeJeu.setMessageLvl(Level.FINER);
+        MoteurDeJeu.setMessageLvl(Level.INFO);
         Statistique statistique = new Statistique();
 
         System.out.println("Meilleur Bot Contre default Bots");
