3c04a6fd2e58913c2f87043ec5c54db9107bb417
unknown <narutovinh@hotmail.fr>
2022-01-05 17:38:27 +0100

    Ajout de tests pour la stratégie 'Agressif' #62

--- src/test/java/fr/unice/polytech/strategiestest/AgressifTest.java
+++ src/test/java/fr/unice/polytech/strategiestest/AgressifTest.java
@@ -2,6 +2,8 @@ package fr.unice.polytech.strategiestest;
 
 import fr.unice.polytech.Joueur;
 import fr.unice.polytech.MoteurDeJeu;
+import fr.unice.polytech.cartes.CartePersonnage;
+import fr.unice.polytech.cartes.CarteQuartier;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 
@@ -42,4 +44,92 @@ public class AgressifTest {
         agressif.piocherPersonnage();
         assertEquals("Condottiere", agressif.getPersonnage().getNom());
     }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void choixMagicien() {
+        ArrayList<CarteQuartier> mainVide = new ArrayList<>();
+        agressif.setQuartiers(mainVide);
+        agressif.piocherPersonnage();
+        assertEquals("Magicien", agressif.getPersonnage().getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void choixVoleur() {
+        Joueur tropRiche = joueurs.get(0);
+        tropRiche.ajouteOr(6);
+        agressif.piocherPersonnage();
+        assertEquals("Voleur", agressif.getPersonnage().getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void tuerVoleur() {
+        Joueur riche = joueurs.get(0);
+        agressif.piocherPersonnage();
+        assertEquals("Assassin", agressif.getPersonnage().getNom());
+        riche.ajouteOr(6);
+        CartePersonnage cible = agressif.getStrategie().getIStrategie().choixDeCibleCartePersonnage(agressif,MoteurDeJeu.deck.getPersonnages());
+        assertEquals("Voleur", cible.getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void tuerCondottiere() {
+        Joueur bientotGagnant = joueurs.get(0);
+        agressif.piocherPersonnage();
+        assertEquals("Assassin", agressif.getPersonnage().getNom());
+        ArrayList<CarteQuartier> quartiersConstruits = new ArrayList<>();
+        for (int i=0; i<7;i++) quartiersConstruits.add(bientotGagnant.piocherQuartier());
+        bientotGagnant.setQuartiersConstruits(quartiersConstruits);
+        assertEquals(7, bientotGagnant.getQuartiersConstruits().size());
+        CartePersonnage cible = agressif.getStrategie().getIStrategie().choixDeCibleCartePersonnage(agressif,MoteurDeJeu.deck.getPersonnages());
+        assertEquals("Condottiere", cible.getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void tuerCondottiereBis() {
+        agressif.piocherPersonnage();
+        assertEquals("Assassin", agressif.getPersonnage().getNom());
+        ArrayList<CarteQuartier> quartiersConstruits = new ArrayList<>();
+        for (int i=0; i<6;i++) quartiersConstruits.add(agressif.piocherQuartier());
+        agressif.setQuartiersConstruits(quartiersConstruits);
+        assertEquals(6, agressif.getQuartiersConstruits().size());
+        CartePersonnage cible = agressif.getStrategie().getIStrategie().choixDeCibleCartePersonnage(agressif,MoteurDeJeu.deck.getPersonnages());
+        assertEquals("Condottiere", cible.getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void tuerRoi() {
+        Joueur roi = joueurs.get(0);
+        agressif.piocherPersonnage();
+        assertEquals("Assassin", agressif.getPersonnage().getNom());
+        ArrayList<CarteQuartier> quartiersConstruits = new ArrayList<>();
+        for (int i=0; i<6;i++) quartiersConstruits.add(roi.piocherQuartier());
+        roi.setQuartiersConstruits(quartiersConstruits);
+        assertEquals(6, roi.getQuartiersConstruits().size());
+        CartePersonnage cible = agressif.getStrategie().getIStrategie().choixDeCibleCartePersonnage(agressif,MoteurDeJeu.deck.getPersonnages());
+        assertEquals("Roi", cible.getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void tuerArchitecte() {
+        Joueur architecte = joueurs.get(0);
+        agressif.piocherPersonnage();
+        assertEquals("Assassin", agressif.getPersonnage().getNom());
+        ArrayList<CarteQuartier> quartiersConstruits = new ArrayList<>();
+        for (int i=0; i<5;i++) quartiersConstruits.add(architecte.piocherQuartier());
+        architecte.setQuartiersConstruits(quartiersConstruits);
+        assertEquals(5, architecte.getQuartiersConstruits().size());
+        CartePersonnage cible = agressif.getStrategie().getIStrategie().choixDeCibleCartePersonnage(agressif,MoteurDeJeu.deck.getPersonnages());
+        assertEquals("Architecte", cible.getNom());
+    }
+
+    @RepeatedTest(MoteurDeJeu.iterationTest)
+    void tuerArchitecteBis() {
+        Joueur architecte = joueurs.get(0);
+        agressif.piocherPersonnage();
+        assertEquals("Assassin", agressif.getPersonnage().getNom());
+        architecte.ajouteOr(4);
+        CartePersonnage cible = agressif.getStrategie().getIStrategie().choixDeCibleCartePersonnage(agressif,MoteurDeJeu.deck.getPersonnages());
+        assertEquals("Architecte", cible.getNom());
+    }
+
 }
